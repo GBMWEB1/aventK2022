fa2faaafc5fbed971ad699b6608ef70cfc251d58
martwebs <mwebster1973@gmail.com>
2022-12-08 09:04:18 +0000

    Day 8

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -1,11 +1,127 @@
+
 class Day8 {
 
+    class Grid (input : List<String>){
+        private val cells = input
+            .map { line ->
+                line.toCharArray()
+                    .map { it.digitToInt() }
+                    .toList() }
+
+        fun getRowCount(): Int{
+            return cells.size
+        }
+        fun getColumnCount(): Int{
+            return cells[0].size
+        }
+
+        fun getCell(row: Int, col: Int): Int{
+            return cells[row][col]
+        }
+
+        private fun getRow(row:Int) : List<Int>{
+            return cells[row]
+        }
+
+        private fun getColumn(col:Int) : List<Int>{
+            return cells.map { it[col] }
+        }
+
+        fun getTop(row: Int, col: Int) : List<Int> {
+            return getColumn(col).subList(0, row)
+        }
+
+        fun getBottom(row: Int, col: Int) : List<Int> {
+            return getColumn(col).subList(row+1, getRowCount())
+        }
+
+        fun getLeft(row: Int, col: Int) : List<Int> {
+            return getRow(row).subList(0, col)
+        }
+
+        fun getRight(row: Int, col: Int) : List<Int> {
+            return getRow(row).subList(col+1, getColumnCount())
+        }
+
+        fun isVisibleTop(row: Int, col: Int) : Boolean {
+            val tree = getCell(row, col)
+            return getTop(row, col).none { it >= tree }
+        }
+
+        fun isVisibleLeft(row: Int, col: Int) : Boolean {
+            val tree = getCell(row, col)
+            return getLeft(row, col).none { it >= tree }
+        }
+
+        fun isVisibleRight(row: Int, col: Int) : Boolean {
+            val tree = getCell(row, col)
+            return getRight(row, col).none { it >= tree }
+        }
+
+        fun isVisibleBottom(row: Int, col: Int) : Boolean {
+            val tree = getCell(row, col)
+            return getBottom(row, col).none { it >= tree }
+        }
+
+        private fun countScore(tree: Int, otherTrees: List<Int>): Int{
+            var score = 0
+            for (cell in otherTrees){
+                if (cell< tree){
+                    score++
+                } else{
+                    score++
+                    return score
+                }
+            }
+            return score
+        }
+
+        fun getScenicScore(row: Int, col: Int): Int {
+            val tree = getCell(row, col)
+            val leftScore = countScore(tree, getLeft(row, col).reversed())
+            val rightScore = countScore(tree, getRight(row, col))
+            val topScore = countScore(tree, getTop(row, col).reversed())
+            val bottomScore = countScore(tree, getBottom(row, col))
+            return leftScore * rightScore * topScore * bottomScore
+        }
+
+    }
 
     fun getPart1(commands: List<String>): Int{
-        return 0
+        return countEdges(commands) + countInterior(commands)
     }
-    fun getPart2(commands: List<String>): Int{
-        return 0
+    fun getPart2(input: List<String>): Int{
+        val grid = Grid(input)
+        var maxScore = 0
+        for (row in 0 until grid.getRowCount()) {
+            for (col in 0 until grid.getColumnCount()) {
+              maxScore = maxOf(maxScore, grid.getScenicScore(row,col))
+            }
+        }
+        return maxScore
     }
 
+    fun countEdges(input: List<String>): Int {
+        val grid = Grid(input)
+        return (grid.getRowCount() *2) + (grid.getColumnCount() *2) - 4
+    }
+
+    fun countInterior(input: List<String>): Int {
+        val grid = Grid(input)
+        var visibleCount = 0
+
+        // Loop through each interior element
+        for (row in 1 .. grid.getRowCount()-2 ){
+            for (col in 1 .. grid.getColumnCount()-2 ){
+                if (grid.isVisibleLeft(row, col)
+                    || grid.isVisibleRight(row, col)
+                    || grid.isVisibleTop(row, col)
+                    || grid.isVisibleBottom(row, col)
+                ){
+                    visibleCount++
+                }
+            }
+        }
+        return visibleCount
+    }
 }
--- src/test/kotlin/Day8Test.kt
+++ src/test/kotlin/Day8Test.kt
@@ -1,32 +1,110 @@
-import old.Day7
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day8Test {
 
     @Test
-    fun testPart1S() {
+    fun testPart1CountEdges() {
         val data = Util().readData("day8-1.txt")
-        assertEquals(0, Day8().getPart1(data))
+        assertEquals(16, Day8().countEdges(data))
+    }
+
+    @Test
+    fun testBuildGrid() {
+        val result = Day8.Grid(listOf("123","567","789"))
+        assertEquals(1, result.getCell(0,0))
+        assertEquals(5, result.getCell(1,0))
+    }
+
+    private val sampleData = listOf(
+        "30373",
+        "25512",
+        "65332",
+        "33549",
+        "35390"
+    )
+    @Test
+    fun testInnerLists() {
+        assertEquals(listOf(2,5,5), Day8.Grid(sampleData).getLeft(1,3))
+        assertEquals(listOf(2), Day8.Grid(sampleData).getRight(1,3))
+        assertEquals(listOf(7), Day8.Grid(sampleData).getTop(1,3))
+        assertEquals(listOf(3,4,9), Day8.Grid(sampleData).getBottom(1,3))
+    }
+
+    @Test
+    fun testTopLeft5() {
+        assertEquals(true, Day8.Grid(sampleData).isVisibleLeft(1,1))
+        assertEquals(true, Day8.Grid(sampleData).isVisibleTop(1,1))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleRight(1,1))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleBottom(1,1))
+    }
+    @Test
+    fun testTopMiddle5() {
+        assertEquals(false, Day8.Grid(sampleData).isVisibleLeft(1,2))
+        assertEquals(true, Day8.Grid(sampleData).isVisibleTop(1,2))
+        assertEquals(true, Day8.Grid(sampleData).isVisibleRight(1,2))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleBottom(1,2))
+    }
+
+    @Test
+    fun testTopRight1() {
+        assertEquals(false, Day8.Grid(sampleData).isVisibleLeft(1,3))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleTop(1,3))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleRight(1,3))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleBottom(1,3))
+    }
+
+    @Test
+    fun testLeftMiddle5() {
+        assertEquals(false, Day8.Grid(sampleData).isVisibleLeft(2,1))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleTop(2,1))
+        assertEquals(true, Day8.Grid(sampleData).isVisibleRight(2,1))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleBottom(2,1))
+    }
+
+    @Test
+    fun testCenter() {
+        assertEquals(false, Day8.Grid(sampleData).isVisibleLeft(2,2))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleTop(2,2))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleRight(2,2))
+        assertEquals(false, Day8.Grid(sampleData).isVisibleBottom(2,2))
+    }
+
+    @Test
+    fun testCountInterior() {
+        assertEquals(1, Day8().countInterior(listOf("123","456","789")))
+    }
+
+
+    @Test
+    fun testPart1CountInterior() {
+        val data = Util().readData("day8-1.txt")
+        assertEquals(5, Day8().countInterior(data))
     }
 
     @Test
     fun testPart1() {
         val data = Util().readData("day8-2.txt")
-        assertEquals(0, Day8().getPart1(data))
+        assertEquals(1698, Day8().getPart1(data))
     }
 
     @Test
-    fun testPart2S() {
-        val data = Util().readData("day8-1.txt")
-
-        assertEquals(0, Day8().getPart2(data))
+    fun testScenicScore() {
+        val sampleData = listOf(
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"
+        )
+        assertEquals(4, Day8.Grid(sampleData).getScenicScore(1,2));
+        assertEquals(8, Day8.Grid(sampleData).getScenicScore(3,2))
     }
 
     @Test
     fun testPart2() {
         val data = Util().readData("day8-2.txt")
-
-        assertEquals(0, Day8().getPart2(data))
+        assertEquals(672280, Day8().getPart2(data))
     }
+
 }
