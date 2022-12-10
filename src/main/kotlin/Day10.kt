3f2e4c4edff73e37cc93dc0f07458e74f0baa81d
martwebs <mwebster1973@gmail.com>
2022-12-10 06:48:06 +0000

    Day10.1

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,2 +1,47 @@
+
+
 class Day10 {
+
+    class CPU (instructionsList: List<String>){
+        var x = 1
+        var cyclesExecuted =0;
+        val instructions = ArrayDeque(instructionsList)
+        var processing = false;
+        var nextValue = 0;
+        fun popInstruction(): String {
+            return instructions.removeFirst();
+        }
+
+        fun signalStrength(): Int{
+            return x * cyclesExecuted;
+        }
+        fun cycle(times: Int){
+            repeat(times){
+                cycle();
+            }
+        }
+        fun cycle(){
+            if (processing) {
+                x+= nextValue
+                nextValue = 0;
+                processing = false;
+
+            } else {
+                val instruction = popInstruction().split(" ");
+                when (instruction[0]){
+                    "noop" -> noop();
+                    "addx" -> addx(instruction[1].toInt())
+                    else -> throw Exception("unknown instruction $instruction")
+                }
+            }
+            cyclesExecuted++;
+        }
+        fun noop(){
+        }
+
+        fun addx(value: Int) {
+            processing = true;
+            nextValue = value;
+        }
+    }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -7,6 +7,61 @@ import org.junit.jupiter.api.Test
 class Day10Test {
 
 
+    @Test
+    fun testNoop(){
+        val cpu = Day10.CPU(listOf("noop"))
+        cpu.cycle()
+        assertEquals(1, cpu.x)
+        assertEquals(1, cpu.cyclesExecuted)
+    }
+
+    @Test
+    fun testSecondCycle(){
+        val cpu = Day10.CPU(listOf("noop","addx 3"))
+        cpu.cycle(2)
+        assertEquals(1, cpu.x)
+        assertEquals(2, cpu.cyclesExecuted)
+    }
+
+    @Test
+    fun testThirdCycle(){
+        val cpu = Day10.CPU(listOf("noop","addx 3"))
+        cpu.cycle(3)
+        assertEquals(4, cpu.x)
+        assertEquals(3, cpu.cyclesExecuted)
+    }
+
+    @Test
+    fun testFithCycle(){
+        val cpu = Day10.CPU(listOf("noop","addx 3", "addx -5"))
+        cpu.cycle(5)
+
+        assertEquals(-1, cpu.x)
+        assertEquals(5, cpu.cyclesExecuted)
+    }
+
+    @Test
+    fun testLargeSample(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(20)
+        assertEquals(420, cpu.signalStrength())
+
+        cpu.cycle(40)
+        assertEquals(1140, cpu.signalStrength())
+
+        cpu.cycle(40)
+        assertEquals(1800, cpu.signalStrength())
+
+        cpu.cycle(40)
+        assertEquals(2940, cpu.signalStrength())
+
+        cpu.cycle(40)
+        assertEquals(2880, cpu.signalStrength())
+
+        cpu.cycle(40)
+        assertEquals(3960, cpu.signalStrength())
+    }
+
 //    @Test
 //    fun testPart1() {
 //        val data = Util().readData("day9-1.txt")
