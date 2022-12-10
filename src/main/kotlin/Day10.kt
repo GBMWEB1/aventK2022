dd2b329af6f580ae77671d0464883c76ac3d8daf
martwebs <mwebster1973@gmail.com>
2022-12-10 09:02:46 +0000

    Day 10 - Complete

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,47 +1,76 @@
-
+import kotlin.math.abs
 
 class Day10 {
 
-    class CPU (instructionsList: List<String>){
-        var x = 1
-        var cyclesExecuted =0;
-        val instructions = ArrayDeque(instructionsList)
-        var processing = false;
-        var nextValue = 0;
-        fun popInstruction(): String {
-            return instructions.removeFirst();
+    class CPU (instructionsList: List<String>, var x: Int = 1, var cyclesExecuted: Int =0){
+
+        private val crtChars = mutableListOf<Char>()
+        private val instructions = processInstructions(instructionsList)
+
+        private var signal = 0
+
+        // expandTheListOfInstructions to include "noop" before all "addx". Moved up for readability
+        private fun processInstructions(input: List<String> ): ArrayDeque<String>{
+
+            val result = ArrayDeque<String>()
+
+            input.forEach {
+                if (it.startsWith("addx")){
+                    result.add("noop")
+                }
+                result.add(it)
+            }
+            return result
         }
 
-        fun signalStrength(): Int{
-            return x * cyclesExecuted;
+        fun cycle(){
+            crtChars.add(getCrtChar())
+
+            cyclesExecuted++
+            signal = x * cyclesExecuted
+
+            val instruction = popInstruction().split(" ")
+            when (instruction[0]){
+                "noop" -> {}
+                "addx" -> { x += instruction[1].toInt() }
+                else -> throw Exception("Unknown instruction $instruction")
+            }
+        }
+
+        private fun getCrtChar(): Char{
+            val pixelMiddle = cyclesExecuted % 40
+            // sprint is 3 character wide
+            if ( abs(pixelMiddle-x) <2){
+                return '#'
+            }
+            return '.'
+        }
+
+        private fun popInstruction(): String {
+            return instructions.removeFirst()
+        }
+
+        fun cycleUntilEnd(){
+            while (instructions.isNotEmpty()){
+                cycle()
+            }
         }
         fun cycle(times: Int){
             repeat(times){
-                cycle();
+                cycle()
             }
         }
-        fun cycle(){
-            if (processing) {
-                x+= nextValue
-                nextValue = 0;
-                processing = false;
 
-            } else {
-                val instruction = popInstruction().split(" ");
-                when (instruction[0]){
-                    "noop" -> noop();
-                    "addx" -> addx(instruction[1].toInt())
-                    else -> throw Exception("unknown instruction $instruction")
-                }
-            }
-            cyclesExecuted++;
-        }
-        fun noop(){
+        fun signalStrength(): Int{
+            return signal
         }
 
-        fun addx(value: Int) {
-            processing = true;
-            nextValue = value;
+        fun getCrtRow(): String{
+            return crtChars.joinToString("")
+        }
+
+        fun getCrtRows(): List<String> {
+            return getCrtRow().chunked(40)
         }
     }
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -1,6 +1,3 @@
-import old.Day9
-import old.Day9.Pos
-
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -57,10 +54,149 @@ class Day10Test {
 
         cpu.cycle(40)
         assertEquals(2880, cpu.signalStrength())
+        assertEquals(16, cpu.x)
 
         cpu.cycle(40)
         assertEquals(3960, cpu.signalStrength())
     }
+
+    @Test
+    fun testPart1(){
+        val cpu = Day10.CPU(Util().readData("day10-3.txt"))
+        cpu.cycle(20)
+        val a = cpu.signalStrength()
+
+        cpu.cycle(40)
+        val b = cpu.signalStrength()
+
+        cpu.cycle(40)
+        val c = cpu.signalStrength()
+
+        cpu.cycle(40)
+        val d = cpu.signalStrength()
+
+        cpu.cycle(40)
+        val e = cpu.signalStrength()
+
+        cpu.cycle(40)
+        val f = cpu.signalStrength()
+
+        assertEquals(14620, a+b+c+d+e+f)
+    }
+
+    @Test
+    fun testCrt(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle()
+        assertEquals("#", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt2(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(2)
+        assertEquals("##", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt3(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(3)
+        assertEquals("##.", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt4(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(4)
+        assertEquals("##..", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt5(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(5)
+        assertEquals("##..#", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt6(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(6)
+        assertEquals("##..##", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt7(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(7)
+        assertEquals("##..##.", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt8(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(8)
+        assertEquals("##..##..", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt9(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(9)
+        assertEquals("##..##..#", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt10(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(10)
+        assertEquals("##..##..##", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt11(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(11)
+        assertEquals("##..##..##.", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt12(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(12)
+        assertEquals("##..##..##..", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrt21(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycle(21)
+        assertEquals("##..##..##..##..##..#", cpu.getCrtRow())
+    }
+
+    @Test
+    fun testCrtSample(){
+        val cpu = Day10.CPU(Util().readData("day10-1.txt"))
+        cpu.cycleUntilEnd()
+        val expect = listOf(
+            "##..##..##..##..##..##..##..##..##..##..",
+            "###...###...###...###...###...###...###.",
+            "####....####....####....####....####....",
+            "#####.....#####.....#####.....#####.....",
+            "######......######......######......####",
+            "#######.......#######.......#######....."
+        )
+        assertEquals(expect, cpu.getCrtRows())
+    }
+
+    @Test
+    fun testPart2() {
+        val cpu = Day10.CPU(Util().readData("day10-3.txt"))
+        cpu.cycleUntilEnd()
+        cpu.getCrtRows().forEach { println(it) }
+    }
+
 
 //    @Test
 //    fun testPart1() {
