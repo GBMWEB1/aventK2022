215e1075a710c3b7f5491aea48d7efbc0e22a82e
martwebs <mwebster1973@gmail.com>
2022-12-11 08:58:49 +0000

    Day 11- start

---
+++ src/main/kotlin/Day11.kt
@@ -0,0 +1,133 @@
+import old.chunk
+import java.lang.Exception
+import java.math.BigInteger
+
+class Day11 {
+
+    data class Operation(val instruction: String){
+
+        fun perform(value: BigInteger): BigInteger {
+            if (instruction== "old * old"){
+                return value * value
+            }
+            if (instruction.startsWith("old *")){
+                return value * BigInteger(instruction.split(" ")[2])
+            }
+            if (instruction.startsWith("old +")) {
+                return value + BigInteger(instruction.split(" ")[2])
+            }
+            throw Exception("Unknown Operation $instruction")
+        }
+
+        fun performInt(value: Long): Int{
+            return perform(BigInteger.valueOf(value)).toInt();
+        }
+
+        companion object{
+            fun of(line:String): Operation{
+                return Operation(line.substring(19))
+            }
+        }
+    }
+
+    class Monkey(itemList: List<BigInteger>,
+                 val operation: Operation,
+                 val testDiv: BigInteger,
+                 val monkeyTrueIndex: Int,
+                 val monkeyFalseIndex: Int,
+                 private val reduceWorry: Boolean
+     ){
+
+        private lateinit var monkeyFalse: Monkey
+        private lateinit var monkeyTrue: Monkey
+
+        private val items = ArrayDeque(itemList)
+        var itemsProcessed = 0
+        fun getItems(): List<BigInteger>{
+            return items.toList()
+        }
+
+        fun getItemsInt(): List<Int>{
+            return items.toList().map { it.toInt() }
+        }
+        companion object{
+            fun of(lines: List<String>, reduceWorry: Boolean): Monkey{
+                val items = lines[1]
+                    .substring(18)
+                    .split(", ")
+                    .map { it.toBigInteger() }
+                    .toList()
+
+                return Monkey(
+                    items,
+                    Operation.of(lines[2]),
+                    BigInteger(lines[3].substring(21)),
+                    lines[4].substring(29).toInt(),
+                    lines[5].substring(30).toInt(),
+                    reduceWorry
+                )
+            }
+            fun getBored(item: BigInteger):BigInteger {
+                return item / BigInteger("3")
+            }
+        }
+
+        fun link(monkeys: List<Monkey>) {
+            monkeyTrue = monkeys[monkeyTrueIndex]
+            monkeyFalse = monkeys[monkeyFalseIndex]
+        }
+
+        fun processAllItems(){
+            while (items.isNotEmpty()){
+                processItem(items.removeFirst())
+            }
+        }
+
+        private fun processItem(item : BigInteger){
+            var item = operation.perform(item)
+            if (reduceWorry) {
+                item = getBored(item)
+            }
+            val mod = item % testDiv
+            if ( mod.toInt() == 0){
+                monkeyTrue.addItem(item)
+            } else{
+                monkeyFalse.addItem(item)
+            }
+            itemsProcessed++
+        }
+
+        private fun addItem(item: BigInteger) {
+            items.add(item)
+        }
+    }
+
+    fun createMonkeys(input: List<String>, reduceWorry: Boolean = true): List<Monkey>{
+        val monkeys = input
+            .filter { it.isNotEmpty() }
+            .chunk(6)
+            .map { Monkey.of(it, reduceWorry) }
+
+        monkeys.forEach { it.link(monkeys) }
+        return monkeys
+    }
+    fun processRound(monkeys : List<Monkey>){
+        monkeys.forEach { it.processAllItems() }
+    }
+
+    fun processRounds(count: Int, monkeys : List<Monkey>){
+        var round = 0;
+        repeat(count){
+            println(round++)
+            processRound(monkeys)
+        }
+    }
+
+    fun getMonkeyBusiness(monkeys : List<Monkey>): Int{
+        return monkeys.map { it.itemsProcessed }
+            .sortedDescending()
+            .take(2)
+            .let { it[0] * it[1] }
+    }
+
+}
---
+++ src/test/kotlin/Day11Test.kt
@@ -0,0 +1,157 @@
+import old.Day10
+import old.chunk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+class Day11Test {
+
+    @Test
+    fun testMonkeyCount() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        assertEquals(4, monkeys.size)
+    }
+
+    @Test
+    fun testParseMonkeyItems() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        assertEquals(listOf(79,98), monkeys[0].getItemsInt())
+    }
+
+    @Test
+    fun testParseMonkeyOperation() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        assertEquals(Day11.Operation("old * 19"), monkeys[0].operation)
+    }
+
+    @Test
+    fun testParseMonkeyTestDiv() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        assertEquals(BigInteger.valueOf(23), monkeys[0].testDiv)
+    }
+
+    @Test
+    fun testParseMonkeyTrueIndex() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        assertEquals(2, monkeys[0].monkeyTrueIndex)
+    }
+
+    @Test
+    fun testParseMonkeyFalseIndex() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        assertEquals(3, monkeys[0].monkeyFalseIndex)
+    }
+
+    @Test
+    fun testOperationTimesN() {
+        assertEquals(38, Day11.Operation("old * 19").performInt(2))
+    }
+
+    @Test
+    fun testOperationPlusN() {
+        assertEquals(21, Day11.Operation("old + 19").performInt(2))
+    }
+
+    @Test
+    fun testOperationSquare() {
+        assertEquals(9, Day11.Operation("old * old").performInt(3))
+    }
+
+    @Test
+    fun testBored() {
+        assertEquals(BigInteger.valueOf(500), Day11.Monkey.getBored(BigInteger.valueOf(1501)))
+        assertEquals(BigInteger.valueOf(620), Day11.Monkey.getBored(BigInteger.valueOf(1862)))
+        assertEquals(BigInteger.valueOf(20), Day11.Monkey.getBored(BigInteger.valueOf(60)))
+    }
+
+    @Test
+    fun testRound1() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        Day11().processRound(monkeys);
+
+        assertEquals(listOf(20,23,27,26), monkeys[0].getItemsInt())
+        assertEquals(listOf(2080,25,167,207,401, 1046), monkeys[1].getItemsInt())
+        assertEquals(listOf<Int>(), monkeys[2].getItemsInt())
+        assertEquals(listOf<Int>(), monkeys[3].getItemsInt())
+    }
+
+    @Test
+    fun testRound20() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        Day11().processRounds(20, monkeys);
+
+        assertEquals(listOf<Int>(10, 12, 14, 26, 34), monkeys[0].getItemsInt())
+        assertEquals(listOf<Int>(245, 93, 53, 199, 115), monkeys[1].getItemsInt())
+        assertEquals(listOf<Int>(), monkeys[2].getItemsInt())
+        assertEquals(listOf<Int>(), monkeys[3].getItemsInt())
+    }
+    @Test
+    fun testRound20ItemsProcessed() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        Day11().processRounds(20, monkeys);
+
+        assertEquals(101, monkeys[0].itemsProcessed)
+        assertEquals(95, monkeys[1].itemsProcessed)
+        assertEquals(7, monkeys[2].itemsProcessed)
+        assertEquals(105, monkeys[3].itemsProcessed)
+    }
+
+    @Test
+    fun testRound20MonkeyBusiness() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"))
+        Day11().processRounds(20, monkeys);
+        assertEquals(10605, Day11().getMonkeyBusiness(monkeys))
+    }
+
+    @Test
+    fun testPart1() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-2.txt"))
+        Day11().processRounds(20, monkeys);
+        assertEquals(54752, Day11().getMonkeyBusiness(monkeys))
+    }
+
+    @Test
+    fun testPart2Round1() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"), false)
+        Day11().processRounds(1, monkeys);
+
+        assertEquals(2, monkeys[0].itemsProcessed)
+        assertEquals(4, monkeys[1].itemsProcessed)
+        assertEquals(3, monkeys[2].itemsProcessed)
+        assertEquals(6, monkeys[3].itemsProcessed)
+    }
+
+    @Test
+    fun testPart2Round20() {
+        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"), false)
+        Day11().processRounds(20, monkeys);
+
+        assertEquals(99, monkeys[0].itemsProcessed)
+        assertEquals(97, monkeys[1].itemsProcessed)
+        assertEquals(8, monkeys[2].itemsProcessed)
+        assertEquals(103, monkeys[3].itemsProcessed)
+    }
+
+//    @Test
+//    fun testPart2Round1000() {
+//        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"), false)
+//        Day11().processRounds(1000, monkeys);
+//
+//        assertEquals(5204, monkeys[0].itemsProcessed)
+//        assertEquals(4792, monkeys[1].itemsProcessed)
+//        assertEquals(199, monkeys[2].itemsProcessed)
+//        assertEquals(5192, monkeys[3].itemsProcessed)
+//    }
+
+//    @Test
+//    fun testPart2Round10000() {
+//        val monkeys = Day11().createMonkeys(Util().readData("day11-1.txt"), false)
+//        Day11().processRounds(10_000, monkeys);
+//
+//        assertEquals(52166, monkeys[0].itemsProcessed)
+//        assertEquals(47830, monkeys[1].itemsProcessed)
+//        assertEquals(1938, monkeys[2].itemsProcessed)
+//        assertEquals(52013, monkeys[3].itemsProcessed)
+//    }
+
+}
