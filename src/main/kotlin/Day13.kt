fa7e4341def79e51c49b25b665a8affc140795e3
martwebs <mwebster1973@gmail.com>
2022-12-13 21:21:56 +0000

    Day 13 - Complete

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,2 +1,109 @@
-class Day13 {
+class Day13(private val debug : Boolean = false) {
+    data class Packet(var value: String){
+        fun parse(): List<Packet>{
+            val result = mutableListOf<Packet>()
+            val stringToSplit = (value.substring(1).substring(0,value.length-2))
+
+            if (stringToSplit.isEmpty()){
+                return listOf()
+            }
+            var charactersSoFar = mutableListOf<Char>()
+            var innerCount = 0
+            for (character in stringToSplit){
+                if (character == '['){
+                    innerCount++
+                }
+                if (character == ']'){
+                    innerCount--
+                }
+                if (innerCount==0 && character== ','){
+                    result.add(Packet(charactersSoFar.joinToString("")))
+                    charactersSoFar = mutableListOf()
+                } else{
+                    charactersSoFar.add(character)
+                }
+            }
+            result.add(Packet(charactersSoFar.joinToString("")))
+            return result
+        }
+
+
+        fun isArray(): Boolean{
+            return value.startsWith("[")
+        }
+
+        fun convertToArray(): Packet{
+            return Packet("[$value]")
+        }
+    }
+
+    enum class Answer(val returnValue: Int){
+        UNKNOWN(0),
+        RIGHT_ORDER(-1),
+        WRONG_ORDER(1)
+    }
+
+    fun isInRightOrder(left: String, right: String): Boolean{
+        return Answer.RIGHT_ORDER == compare(Packet(left), Packet(right))
+    }
+
+    fun isInRightOrderCompare(left: String, right: String):Int{
+        return compare(Packet(left), Packet(right)).returnValue
+    }
+
+    fun compare(left: Packet, right: Packet, level: Int=0) : Answer{
+        printPadded(level, "Comparing $left vs $right")
+
+        val rightParts = right.parse()
+        left.parse().forEachIndexed { index, leftPart ->
+            if (index == rightParts.size){
+                printPadded(level+1,"Right side ran out of items, so inputs are in the right order")
+                return Answer.WRONG_ORDER
+            }
+
+            val rightPart = rightParts[index]
+            printPadded(level+1,"Comparing $leftPart vs $rightPart")
+
+            var comparison = Answer.UNKNOWN
+            if (leftPart.isArray() && rightPart.isArray()){
+                comparison = compare(leftPart, rightPart, level+1)
+            }
+            else if (leftPart.isArray()){
+                printPadded(level+1,"Mixed types; convert right to [$rightPart] and retry comparison")
+                comparison = compare(leftPart, rightPart.convertToArray(),level+1)
+            }
+            else if (rightPart.isArray()){
+                printPadded(level+1,"Mixed types; convert left to [$leftPart] and retry comparison")
+                comparison = compare(leftPart.convertToArray(), rightPart, level+1)
+            } else{
+                if (leftPart.value.toInt()> rightPart.value.toInt()){
+                    printPadded(level+2,"Right side is smaller, so inputs are not in the right order")
+                    return Answer.WRONG_ORDER
+                }
+                if (leftPart.value.toInt()< rightPart.value.toInt()){
+                    printPadded(level+2,"Left side is smaller, so inputs are in the right order")
+                    return Answer.RIGHT_ORDER
+                }
+            }
+            if (comparison!= Answer.UNKNOWN){
+                return comparison
+            }
+        }
+        if (left.parse().size < rightParts.size){
+            printPadded(level+1,"Left side ran out of items, so inputs are in the right order")
+            return Answer.RIGHT_ORDER
+        }
+
+        if (level==0){
+            return Answer.RIGHT_ORDER
+        }
+        return Answer.UNKNOWN
+    }
+
+    private fun printPadded(level: Int, content: String) {
+        if (debug){
+            val pad = Array(level*2){' '}.joinToString("")
+            println("$pad - $content")
+        }
+    }
 }
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -1,13 +1,124 @@
-import old.Day12
+import Day13.Answer
+import Day13.Packet
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day13Test {
 
     @Test
+    fun testPair1() {
+        assertEquals(Answer.RIGHT_ORDER, Day13().compare(Packet("[1,1,3,1,1]"), Packet("[1,1,5,1,1]")))
+    }
+
+    @Test
+    fun testPair2() {
+        assertEquals(Answer.RIGHT_ORDER, Day13().compare(Packet("[[1],[2,3,4]]"), Packet("[[1],4]")))
+    }
+
+    @Test
+    fun testPair3() {
+        assertEquals(Answer.WRONG_ORDER, Day13().compare(
+            Packet("[9]"), Packet("[[8,7,6]]")))
+    }
+
+    @Test
+    fun testPair4() {
+        assertEquals(Answer.RIGHT_ORDER, Day13().compare(
+            Packet("[[4,4],4,4]"),
+            Packet("[[4,4],4,4,4]")))
+    }
+
+    @Test
+    fun testPair5() {
+        assertEquals(Answer.WRONG_ORDER, Day13().compare(
+            Packet("[7,7,7,7]"), Packet("[7,7,7]"))
+        )
+    }
+
+    @Test
+    fun testPair6() {
+        assertEquals(Answer.RIGHT_ORDER, Day13().compare(
+            Packet("[]"),
+            Packet("[3]")))
+    }
+
+    @Test
+    fun testPair7() {
+        assertEquals(Answer.WRONG_ORDER,
+            Day13().compare(Packet("[[[]]]"), Packet("[[]]")))
+    }
+    @Test
+    fun testPair8() {
+        assertEquals(Answer.WRONG_ORDER,
+            Day13().compare(
+                Packet("[1,[2,[3,[4,[5,6,7]]]],8,9]"),
+                Packet("[1,[2,[3,[4,[5,6,0]]]],8,9]")))
+    }
+
+    @Test
+    fun testPair9() {
+        assertEquals(Answer.RIGHT_ORDER,
+            Day13().compare(Packet("[]"), Packet("[[7],[0,4,6,1]]")))
+    }
+
+    @Test
+    fun testSample() {
+        val input = Util().readData("day13.txt")
+        val pairs = input.filter { it.isNotEmpty() }.chunked(2)
+
+        var sum = 0
+        pairs.forEachIndexed { index, pair ->
+            val pairNumber = index +1
+            println("== Pair $pairNumber ==")
+            if (Day13().isInRightOrder(pair[0], pair[1])){
+                sum += index+1
+            }
+        }
+        assertEquals(13, sum)
+    }
+
+    @Test
     fun testPart1() {
-        val grid = Day12.Grid(Util().readData("day13.txt"))
-            .init()
-       // assertEquals(408, grid.navigateToEnd())
+        val input = Util().readData("day13-2.txt")
+        val pairs = input.filter { it.isNotEmpty() }.chunked(2)
+
+        var sum = 0
+        pairs.forEachIndexed { index, pair ->
+            val pairNumber = index +1
+            println("== Pair $pairNumber ==")
+            if (Day13().isInRightOrder(pair[0], pair[1])){
+                sum += index+1
+            }
+        }
+        assertEquals(5208, sum)
+    }
+
+    @Test
+    fun testPart2Sample() {
+        val list = Util().readData("day13.txt")
+            .filter { it.isNotEmpty() }.toMutableList()
+
+        list.add("[2]")
+        list.add("[6]")
+
+        val sorted = list.sortedWith { o1, o2 -> Day13().isInRightOrderCompare(o1, o2) }
+
+        println(sorted)
+
+    }
+    @Test
+    fun testPart2() {
+        val list = Util().readData("day13-2.txt")
+            .filter { it.isNotEmpty() }.toMutableList()
+
+        list.add("[2]")
+        list.add("[6]")
+        val sorted = list.sortedWith { o1, o2 -> Day13().isInRightOrderCompare(o1, o2) }
+
+        val value = (sorted.indexOf("[2]")+1) * (sorted.indexOf("[6]")+1)
+
+        println(sorted)
+        assertEquals(25792, value)
+
     }
 }
