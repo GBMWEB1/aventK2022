c6c394363282edcd8e4a20ce3a5c14e7c018b58f
martwebs <mwebster1973@gmail.com>
2022-12-14 08:55:05 +0000

    Day 14 - Complete

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,9 +1,173 @@
-class Day14(private val debug : Boolean = false) {
+class Day14 {
 
-    private fun printPadded(level: Int, content: String) {
-        if (debug){
-            val pad = Array(level*2){' '}.joinToString("")
-            println("$pad - $content")
+    class Cave {
+        var maxleft = 500
+        var maxRight = 500
+        var bottomCave = 0
+
+        private val contents = mutableListOf<ArrayDeque<Char>>()
+
+        init {
+            contents.add( ArrayDeque(listOf('+')))
+        }
+        fun addPath(line:String){
+            val points = line.split(" -> ")
+
+            var previousPointX = -1
+            var previousPointY = -1
+
+            for (point in points){
+                var expandLeft = 0
+                var expandRight = 0
+                var expandBottom = 0
+                val x = point.split(",")[0].toInt()
+                val y = point.split(",")[1].toInt()
+                if (x< maxleft){
+                    expandLeft = maxleft-x
+                    maxleft = x
+                }
+                if (x> maxRight){
+                    expandRight = x - maxRight
+                    maxRight = x
+                }
+                if (y> bottomCave){
+                    expandBottom = y - bottomCave
+                    bottomCave = y
+                }
+                expandCave(expandLeft, expandRight, expandBottom)
+                if (previousPointX>0){
+                    drawRockLine(previousPointX, previousPointY, x,y)
+                }
+                previousPointX = x
+                previousPointY = y
+            }
+        }
+
+        private fun drawRockLine(startX: Int, startY: Int, endX: Int, endY: Int) {
+            val lineLeft = minOf(startX, endX) - maxleft
+            val lineRight = maxOf(startX, endX) - maxleft
+            val lineTop = minOf(startY, endY)
+            val lineBottom = maxOf(startY, endY)
+
+            for (rowIndex in lineTop..lineBottom){
+                val row = contents[rowIndex]
+                for (colIndex in lineLeft .. lineRight){
+                    row[colIndex] = '#'
+                }
+            }
+        }
+
+        private fun expandCave(left: Int, right: Int, bottom: Int){
+            repeat(left){
+                for (row in contents){
+                    row.addFirst('.')
+                }
+            }
+            repeat(right){
+                for (row in contents){
+                    row.addLast('.')
+                }
+            }
+            repeat(bottom){
+                val caveWidth = (maxRight - maxleft)+1
+                val row = Array(caveWidth){'.'}.toList()
+                ArrayDeque<Char>()
+                contents.add(ArrayDeque(row))
+            }
+        }
+
+        fun getDisplay(): List<String> {
+            return contents.map { it.joinToString("") }
+        }
+
+        private fun isBlocked(x:Int, y: Int) : Boolean{
+            val cell = contents[y][x-maxleft]
+            if (cell == '#' || cell == 'o'){
+                return true
+            }
+            return false
+        }
+
+        private fun dropSand(currentX: Int, currentY:Int):Boolean{
+            if (contents[currentY][currentX-maxleft] == 'o'){
+               return false
+            }
+            val nextY = currentY+1
+            if (!isBlocked(currentX, nextY)){
+                return dropSand(currentX, nextY)
+            }
+
+            // check for left
+            if (isAbyss(currentX - 1)){
+                return false
+            }
+            if (!isBlocked(currentX - 1, nextY) ){
+                return dropSand(currentX - 1, nextY)
+            }
+
+            if (isAbyss(currentX + 1)){
+                return false
+            }
+            if (!isBlocked(currentX + 1, nextY)){
+                return dropSand(currentX + 1, nextY)
+            }
+            contents[currentY][currentX-maxleft] = 'o'
+            return true
+        }
+
+        private fun isAbyss(x: Int): Boolean {
+            if (x > maxRight){
+                return true
+            }
+            if (x < maxleft){
+                return true
+            }
+            return false
+        }
+
+        fun dropSand(count: Int){
+            repeat(count){
+                dropSand()
+            }
+        }
+        fun dropSand(){
+            val currentX = 500
+            val currentY = 0
+            dropSand(currentX, currentY)
+        }
+
+        fun dropUntilEnd(): Int{
+            var pouring = true
+            var sandDropped = 0
+            while (pouring) {
+                sandDropped++
+                val currentX = 500
+                val currentY = 0
+                pouring = dropSand(currentX, currentY)
+            }
+            return sandDropped-1
+        }
+
+        fun addFloor() {
+            bottomCave += 2
+
+            var newLeft = 500
+            var newRight = 500
+            for (y in 0..bottomCave){
+                newLeft--
+                newRight++
+            }
+            val expandLeft = maxleft - newLeft
+            val expandRight = newRight - maxRight
+
+            maxleft = newLeft
+            maxRight = newRight
+            expandCave(expandLeft,expandRight,2)
+
+            // now set rock on the bottom
+            for (bot in maxleft..maxRight){
+                contents[bottomCave][bot-maxleft] = '#'
+            }
         }
     }
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,3 +1,192 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class Day14Test {
+
+    //498,4 -> 498,6 -> 496,6
+
+    @Test
+    fun testDrawSingle(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        assertEquals(496, cave.maxleft)
+        assertEquals(500, cave.maxRight)
+        assertEquals(6, cave.bottomCave)
+        assertEquals(listOf(
+            "....+",
+            ".....",
+            ".....",
+            ".....",
+            "..#..",
+            "..#..",
+            "###.."), cave.getDisplay())
+    }
+
+    @Test
+    fun testDrawMultiple(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+
+        assertEquals(494, cave.maxleft)
+        assertEquals(503, cave.maxRight)
+        assertEquals(9, cave.bottomCave)
+        assertEquals(listOf(
+        "......+...",
+        "..........",
+        "..........",
+        "..........",
+        "....#...##",
+        "....#...#.",
+        "..###...#.",
+        "........#.",
+        "........#.",
+        "#########."), cave.getDisplay())
+    }
+
+    @Test
+    fun testDropSingle(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        cave.dropSand()
+        assertEquals(listOf(
+            "......+...",
+            "..........",
+            "..........",
+            "..........",
+            "....#...##",
+            "....#...#.",
+            "..###...#.",
+            "........#.",
+            "......o.#.",
+            "#########."), cave.getDisplay())
+    }
+
+    @Test
+    fun testDropTwo(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        cave.dropSand(2)
+
+        assertEquals(listOf(
+            "......+...",
+            "..........",
+            "..........",
+            "..........",
+            "....#...##",
+            "....#...#.",
+            "..###...#.",
+            "........#.",
+            ".....oo.#.",
+            "#########."), cave.getDisplay())
+    }
+
+    @Test
+    fun testDropFive(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        cave.dropSand(5)
+
+        assertEquals(listOf(
+            "......+...",
+            "..........",
+            "..........",
+            "..........",
+            "....#...##",
+            "....#...#.",
+            "..###...#.",
+            "......o.#.",
+            "....oooo#.",
+            "#########."), cave.getDisplay())
+    }
+
+    @Test
+    fun testDrop22(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        cave.dropSand(22)
+
+        assertEquals(listOf(
+        "......+...",
+        "..........",
+        "......o...",
+        ".....ooo..",
+        "....#ooo##",
+        "....#ooo#.",
+        "..###ooo#.",
+        "....oooo#.",
+        "...ooooo#.",
+        "#########."), cave.getDisplay())
+    }
+
+    @Test
+    fun testPart1Sample(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        assertEquals(24,  cave.dropUntilEnd())
+    }
+
+    @Test
+    fun testPart1(){
+        val cave = Day14.Cave()
+        for (line in Util().readData("day14.txt")){
+            cave.addPath(line)
+        }
+        assertEquals(888,  cave.dropUntilEnd())
+    }
+
+    ////////////
+    // Part 2 //
+    ////////////
+    @Test
+    fun testAddFloor(){
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        cave.addFloor()
+
+        val result= listOf(
+        "............+............",
+        ".........................",
+        ".........................",
+        ".........................",
+        "..........#...##.........",
+        "..........#...#..........",
+        "........###...#..........",
+        "..............#..........",
+        "..............#..........",
+        "......#########..........",
+        ".........................",
+        "#########################")
+
+        assertEquals(11, cave.bottomCave)
+        assertEquals(494-6, cave.maxleft)
+        assertEquals(503+9, cave.maxRight)
+        assertEquals(result,cave.getDisplay())
+    }
+
+    @Test
+    fun testFillCave() {
+        val cave = Day14.Cave()
+        cave.addPath("498,4 -> 498,6 -> 496,6")
+        cave.addPath("503,4 -> 502,4 -> 502,9 -> 494,9")
+        cave.addFloor()
+        assertEquals(93, cave.dropUntilEnd())
+    }
+
+    @Test
+    fun testPart2(){
+        val cave = Day14.Cave()
+        for (line in Util().readData("day14.txt")){
+            cave.addPath(line)
+        }
+        cave.addFloor()
+        assertEquals(26461,  cave.dropUntilEnd())
+    }
 
 }
