be78b77bae6f22177ee29e97cf7eed5ce4cfe522
martwebs <mwebster1973@gmail.com>
2022-12-15 10:16:07 +0000

    Day 15 - Prep

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,4 +1,116 @@
+import kotlin.math.abs
+
 class Day15 {
 
+    data class Beacon(val x :Int, val y: Int )
+
+    data class Sensor (val x: Int, val y:Int, val beacon: Beacon){
+        fun distance(): Int{
+            return  abs(x-beacon.x) + abs(y-beacon.y)
+        }
+
+        fun topX(): Int{
+            return beacon.x - distance();
+        }
+
+        fun topY(): Int{
+            return beacon.y - distance();
+        }
+
+        fun bottomX(): Int{
+            return beacon.x + distance();
+        }
+
+        fun bottomY(): Int{
+            return beacon.y + distance();
+        }
+
+        fun isInArea(posX: Int, posY: Int): Boolean{
+
+            // calculate the distance from the signal
+            val posDistance= abs(posX - x) + abs(posY-y)
+
+            return posDistance <= distance();
+        }
+
+        fun isSensor(posX: Int, posY: Int): Boolean{
+            return posX == x && posY == y
+        }
+        fun isBeacon(posX: Int, posY: Int): Boolean{
+            return posY == beacon.y && posX == beacon.x
+        }
+
+
+            companion object {
+
+            fun extractX(input: String): Int{
+                return input.substring(
+                    input.indexOf("x=")+2,
+                    input.indexOf(",")).toInt();
+            }
+
+            fun extractY(input: String): Int{
+                return input.substring(
+                    input.indexOf("y=")+2,input.length).toInt()
+            }
+
+
+            fun of(line: String) : Sensor{
+                //Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+                val signalInput = line.split(":")[0]
+                val beaconInput = line.split(":")[1]
+
+                return Sensor(extractX(signalInput), extractY(signalInput),
+                    Beacon(extractX(beaconInput), extractY(beaconInput)))
+            }
+        }
+    }
+
+    class Grid(lines: List<String>){
+        val sensors = lines.map { Sensor.of(it) }
+
+        fun displayAt(x:Int, y:Int): Char{
+            if (sensors.any { it.isSensor(x,y) }){
+                return 'S'
+            }
+            if (sensors.any { it.isBeacon(x,y) }){
+                return 'B'
+            }
+
+            if (sensors.any { it.isInArea(x, y) }) {
+                return '#'
+            }
+            return '.'
+        }
+        fun display() {
+            val startY = minOf(0, sensors.minOf { it.topY() })
+            val endY = sensors.maxOf { it.bottomY() }
+            val startX = sensors.minOf { it.topX() }
+            val endX = sensors.maxOf { it.bottomX() }
+
+            println(" Grid starts at $startX, $startY")
+            for (y in startY..endY){
+                for (x in startX..endX){
+                    print(displayAt(x,y))
+                }
+                println();
+            }
+
+        }
+
+        fun countCoverage(row: Int): Int {
+            val startX = minOf(sensors.minOf { it.topX() })
+            val endX = sensors.maxOf { it.bottomX() }
+
+            var count =0;
+            for (x in startX..endX){
+                if (displayAt(x,row) == '#' || displayAt(x,row) == 'S'){
+                    count++;
+                }
+            }
+            return count;
+
+        }
+    }
 
 }
--- src/test/kotlin/Day15Test.kt
+++ src/test/kotlin/Day15Test.kt
@@ -1,7 +1,73 @@
-import old.Day14
+import Day15.Sensor.Companion.of
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 
 class Day15Test {
 
+    @Test
+    fun testCreate() {
+        val result = of("Sensor at x=2, y=18: closest beacon is at x=-2, y=15")
+        assertEquals(2, result.x)
+        assertEquals(18, result.y)
+        assertEquals(-2, result.beacon.x)
+        assertEquals(15, result.beacon.y)
+    }
+
+    @Test
+    fun testDistance() {
+        assertEquals(3, of("Sensor at x=2, y=18: closest beacon is at x=2, y=15").distance())
+
+        assertEquals(4, of("Sensor at x=0, y=0: closest beacon is at x=4, y=0").distance())
+        assertEquals(4, of("Sensor at x=0, y=0: closest beacon is at x=-4, y=0").distance())
+        assertEquals(4, of("Sensor at x=0, y=0: closest beacon is at x=0, y=4").distance())
+        assertEquals(4, of("Sensor at x=0, y=0: closest beacon is at x=0, y=-4").distance())
+
+        assertEquals(8, of("Sensor at x=0, y=0: closest beacon is at x=4, y=-4").distance())
+        assertEquals(9, of("Sensor at x=8, y=7: closest beacon is at x=2, y=10").distance())
+    }
+
+    @Test
+    fun testIsInArea() {
+        val signal = of("Sensor at x=8, y=7: closest beacon is at x=2, y=10");
+        assertFalse(signal.isInArea(0,0))
+        assertEquals(true, signal.isInArea(8,-2))
+        assertEquals(false, signal.isInArea(8,-3))
+        assertEquals(false, signal.isInArea(8,-3))
+
+        // x of 6, y = 0 is in area
+        assertEquals(true, signal.isInArea(6,0))
+        assertEquals(false, signal.isInArea(5,0))
+        assertEquals(false, signal.isInArea(6,-1))
+    }
+
+    @Test
+    fun testIsBeacon() {
+        val signal = of("Sensor at x=8, y=7: closest beacon is at x=2, y=10");
+        assertEquals(true, signal.isBeacon(2,10))
+    }
+
+
+    @Test
+    fun testDisplay() {
+        val grid = Day15.Grid(listOf("Sensor at x=8, y=7: closest beacon is at x=2, y=10"))
+        grid.display()
+    }
+
+    @Test
+    fun testDisplaySample() {
+        val grid = Day15.Grid(Util().readData("day15.txt"))
+        grid.display();
+    }
+
+    @Test
+    fun testCountCoverage() {
+        assertEquals(26, Day15.Grid(Util().readData("day15.txt")).countCoverage(10));
+
+    }
+
+    @Test
+    fun testPart1() {
+        assertEquals(4961647, Day15.Grid(Util().readData("day15-2.txt")).countCoverage(2000000));
+    }
 }
